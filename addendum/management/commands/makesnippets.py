8af265af0f1d0a7889c299c90ff1f5c4f6f7d81b
Hernan Lozano <hernantz@gmail.com>
2013-06-13 16:18:26 -0300

    PEP8 cleanup.

--- addendum/management/commands/makesnippets.py
+++ addendum/management/commands/makesnippets.py
@@ -7,14 +7,13 @@ from django.conf import settings
 from addendum.models import Snippet
 
 
-
 IS_ADDENDUM = r'\{\% load addendum_tags \%\}'
-SNIPPET_PATTERNS= (
+SNIPPET_PATTERNS = (
     re.compile(r"""\{\% snippet \'(?P<name>\w*:\w*)\' \%\}(?P<content>.*)\{\% endsnippet \%\}""", re.UNICODE),
     re.compile(r"""\{\%snippet \'(?P<name>\w*:\w*)\' \%\}(?P<content>.*)\{\% endsnippet\%\}""", re.UNICODE),
     re.compile(r"""\{\% snippet (?P<name>\w*) \%\}(?P<content>.*)\{\% endsnippet \%\}""", re.UNICODE),
     re.compile(r"""\{\%snippet (?P<name>\w*) \%\}(?P<content>.*)\{\% endsnippet\%\}""", re.UNICODE),
-    )
+)
 
 
 class Command(BaseCommand):
@@ -25,38 +24,33 @@ class Command(BaseCommand):
         for dr in settings.TEMPLATE_DIRS:
             for root, subFolders, files in os.walk(dr):
                 for file in files:
-                    file_list.append(os.path.join(root,file))
+                    file_list.append(os.path.join(root, file))
         self.files = file_list
 
-
     def search_files(self):
-        for template in  self.files:
+        for template in self.files:
             with open(template, 'r') as template:
                 data = template.read()
 
-            is_addendum = re.search(IS_ADDENDUM, data) # check if the template loads addendum
+            # check if the template loads addendum
+            is_addendum = re.search(IS_ADDENDUM, data)
             if is_addendum:
                 for pattern in SNIPPET_PATTERNS:
                     snips = [m.groupdict() for m in pattern.finditer(data)]
                     if snips:
-                        self.founds += snips 
+                        self.founds += snips
 
     def handle_results(self):
         for snip in self.founds:
             snip, c = Snippet.objects.get_or_create(
-                key = snip['name'],
-                defaults={'text':snip['content']}
-                )
-
-
+                key=snip['name'],
+                defaults={'text': snip['content']}
+            )
 
     def handle(self, *args, **options):
-        self.founds = [] # list for storing re.matches
-        
+        self.founds = []  # list for storing re.matches
+
         self.set_files()
         self.search_files()
 
         self.handle_results()
-
-
-
